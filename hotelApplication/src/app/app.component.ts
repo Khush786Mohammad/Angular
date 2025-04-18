2084c4fdd02e6f11f66a6e1d5e7180049e58362d
Khush786Mohammad <khush786moh@gmail.com>
2025-04-18 23:27:22 +0530

    hotel Application

--- hotelApplication/src/app/app.component.ts
+++ hotelApplication/src/app/app.component.ts
@@ -4,8 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  // template: `<h1>hellow World!</h1>`,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'hotelApplication';
