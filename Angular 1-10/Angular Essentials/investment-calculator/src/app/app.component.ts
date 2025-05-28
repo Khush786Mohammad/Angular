b663fba5eab5346d9efcb6e0016d9b866af3d2a4
Khush786Mohammad <khush786moh@gmail.com>
2025-05-28 09:41:16 +0000

    Investment Calculator(NgModule)

--- Angular 1-10/Angular Essentials/investment-calculator/src/app/app.component.ts
+++ Angular 1-10/Angular Essentials/investment-calculator/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component, inject, signal } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from "./user-input/user-input.component";
 import { type investmentData, type dataType } from './modal/data.modules';
-import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 import { InvestmentService } from './investment.service.service';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
-  standalone: true,
+  standalone: false,
   templateUrl: './app.component.html',
 })
 export class AppComponent {
