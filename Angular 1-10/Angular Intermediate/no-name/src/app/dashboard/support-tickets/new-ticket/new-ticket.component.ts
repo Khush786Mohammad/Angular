da153bc5c8b972b085d25b3523cb6128bb0ef1c6
Khush786Mohammad <khush786moh@gmail.com>
2025-05-30 12:09:57 +0000

    no-name

--- Angular 1-10/Angular Intermediate/no-name/src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ Angular 1-10/Angular Intermediate/no-name/src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -4,6 +4,7 @@ import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
   selector: 'app-new-ticket',
+  standalone: true,
   imports: [ButtonComponent, ControlComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
--- Angular 1-10/Angular Intermediate/no-name/src/app/header/header.component.ts
+++ Angular 1-10/Angular Intermediate/no-name/src/app/header/header.component.ts
@@ -3,6 +3,7 @@ import { ButtonComponent } from '../shared/button/button.component';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
