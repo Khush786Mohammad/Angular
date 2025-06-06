b8e6b61ee7aa14beda68c9a8c1a37c546d432a81
Khush786Mohammad <khush786moh@gmail.com>
2025-06-06 12:40:23 +0000

    Directive Deep Dive

--- Angular 1-10/Angular Intermediate/Directive Deep Dive/sample1/src/app/app.component.ts
+++ Angular 1-10/Angular Intermediate/Directive Deep Dive/sample1/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
 
 import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { AuthService } from './auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [AuthComponent, LearningResourcesComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  private authService = inject(AuthService);
+
+  isAdmin = computed(()=>{
+    return this.authService.activePermission() === 'admin';
+  })
+}
