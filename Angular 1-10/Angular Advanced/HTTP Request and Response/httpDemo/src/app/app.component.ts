575918e996cd613ae6fc287fc2f130b0ff437870
Khush786Mohammad <khush786moh@gmail.com>
2025-06-30 22:09:38 +0530

    http

--- Angular 1-10/Angular Advanced/HTTP Request and Response/httpDemo/src/app/app.component.ts
+++ Angular 1-10/Angular Advanced/HTTP Request and Response/httpDemo/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { AvailablePlacesComponent } from './places/available-places/available-places.component';
+import { UserPlacesComponent } from './places/user-places/user-places.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [AvailablePlacesComponent, UserPlacesComponent],
 })
-export class AppComponent {
-  title = 'httpDemo';
-}
+export class AppComponent {}
