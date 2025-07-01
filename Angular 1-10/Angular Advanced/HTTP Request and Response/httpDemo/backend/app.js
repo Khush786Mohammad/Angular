50b2b102032f1579e0d209d54df331d99c1c5964
Khush786Mohammad <khush786moh@gmail.com>
2025-07-01 22:07:40 +0530

    Cors

--- Angular 1-10/Angular Advanced/HTTP Request and Response/httpDemo/backend/app.js
+++ Angular 1-10/Angular Advanced/HTTP Request and Response/httpDemo/backend/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // allow all domains
   res.setHeader("Access-Control-Allow-Methods", "GET, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if(req.method === "OPTIONS"){
     return res.sendStatus(200);
