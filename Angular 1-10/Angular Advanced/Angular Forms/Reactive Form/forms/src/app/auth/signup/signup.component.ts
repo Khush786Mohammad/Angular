225d4196c67282910a471e802543fe9a3f76718e
Khush786Mohammad <khush786moh@gmail.com>
2025-07-08 06:57:04 +0000

    Reactive Forms

--- Angular 1-10/Angular Advanced/Angular Forms/Reactive Form/forms/src/app/auth/signup/signup.component.ts
+++ Angular 1-10/Angular Advanced/Angular Forms/Reactive Form/forms/src/app/auth/signup/signup.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule,Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule,Validators } from '@angular/forms';
 
 function mustContainsSpecialCharacter(control: AbstractControl){
+  if(!control.value)
+    return null;
   const specialCharacters = ['!','@','#','$','%'];
   const doesContains = specialCharacters.some((char) => control.value.includes(char))
   if(doesContains){
     return null;
   }
   return {doesNotContainSpecialCharacters: true};
+}
+
+function passwordMatched(control: AbstractControl){
+  const password = control.get('password')?.value;
+  const confirmPassword = control.get('confirmPassword')?.value;
+
+  if(password === confirmPassword){
+    return null;
+  }
+  return {passwordDoesNotMatched: true};
 }
 
 @Component({
@@ -22,9 +34,51 @@ export class SignupComponent {
     email: new FormControl('',{
       validators: [Validators.required, Validators.email]
     }),
-    password: new FormControl('',{
-      validators: [Validators.required, Validators.minLength(8),mustContainsSpecialCharacter]
-    })
+    passwords: new FormGroup({
+      password: new FormControl('',{
+        validators: [Validators.required, Validators.minLength(8),mustContainsSpecialCharacter]
+      }),
+      confirmPassword: new FormControl('',{
+        validators:[Validators.required,Validators.minLength(8),mustContainsSpecialCharacter]
+      }),
+     },
+     {
+      validators: [passwordMatched]
+     }
+    ),
+    firstName: new FormControl('',{
+      validators: [Validators.required]
+    }),
+    lastName: new FormControl('',{
+      validators: [Validators.required]
+    }),
+
+    address: new FormGroup({
+      street: new FormControl('',{
+        validators: [Validators.required]
+      }),
+      number: new FormControl('',{
+        validators: [Validators.required]
+      }),
+      postalCode: new FormControl('',{
+        validators: [Validators.required]
+      }),
+      city: new FormControl('',{
+        validators: [Validators.required]
+      })
+    }), 
+    
+    role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('other',{
+      validators: [Validators.required]
+    }),
+    source: new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false)
+    ],{
+      validators: [Validators.required]
+    }),
+    agree: new FormControl(false, {validators: [Validators.requiredTrue]})
   });
 
   get isEmailInvalid(){
@@ -32,13 +86,24 @@ export class SignupComponent {
   }
 
   get isPasswordInvalid(){
-    return this.signUp.controls.password.touched && this.signUp.controls.password.dirty && this.signUp.controls.password.invalid;
+    return this.signUp.controls.passwords.controls.password.touched && this.signUp.controls.passwords.controls.password.dirty && this.signUp.controls.passwords.controls.password.invalid;
   }
 
+  get passwordNotMatched(){
+    return this.signUp.controls.passwords.controls.password.touched && 
+    this.signUp.controls.passwords.controls.password.dirty && 
+    this.signUp.controls.passwords.controls.password.valid && 
+    this.signUp.controls.passwords.controls.confirmPassword.touched && 
+    this.signUp.controls.passwords.controls.confirmPassword.dirty && 
+    this.signUp.controls.passwords.controls.password.value !== this.signUp.controls.passwords.controls.confirmPassword.value;
+  }
   onSubmit(){
     console.log(this.signUp);
-    console.log(this.signUp.controls.email.value);
-    console.log(this.signUp.controls.password.value);
+    if(this.signUp.valid){
+      console.log('form is valid');
+    }
+    else
+    console.log("form is invalid")
   }
 
   onReset(){
