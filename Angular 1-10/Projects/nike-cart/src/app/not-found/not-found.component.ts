1589887b05b71ef59f2f9e015b0596563df8cd2d
Khush786Mohammad <khush786moh@gmail.com>
2025-07-22 11:05:59 +0000

    routing

---
+++ Angular 1-10/Projects/nike-cart/src/app/not-found/not-found.component.ts
@@ -0,0 +1,12 @@
+import { NgStyle } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [NgStyle],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.css'
+})
+export class NotFoundComponent {
+
+}
