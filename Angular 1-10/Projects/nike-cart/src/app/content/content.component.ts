60243494261a78a082a625c29332f78c83ea35d7
Khush786Mohammad <khush786moh@gmail.com>
2025-07-24 21:16:22 +0530

    nike

--- Angular 1-10/Projects/nike-cart/src/app/content/content.component.ts
+++ Angular 1-10/Projects/nike-cart/src/app/content/content.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { SideMenuComponent } from "./side-menu/side-menu.component";
-import { MainContentComponent } from './main-content/main-content.component';
-import { CartComponent } from './cart/cart.component';
-import { NgClass } from '@angular/common';
 import { ShoeService } from '../service/shoe.service';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-content',
-  imports: [SideMenuComponent,MainContentComponent,CartComponent, RouterOutlet],
+  imports: [ RouterOutlet],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css',
 })
